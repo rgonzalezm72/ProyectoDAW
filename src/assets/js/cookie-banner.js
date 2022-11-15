73d1479e38e04a8cf81b64990d7e63e43251612c
rgonzalezm72 <rgonzalezm72@iesvjp.es>
2022-11-15 18:03:56 +0100

    Cookie Banner terminado e inclusión de un chat en la sección de Contacto mediante Dead Simple Chat

--- src/assets/js/cookie-banner.js
+++ src/assets/js/cookie-banner.js
@@ -1,38 +1,41 @@
 /*
-* Javascript to show and hide cookie banner using localstorage
+* Javascript para mostrar y ocultar el Cookie Banner usando LocalStorage
 */
 
-/**
- * @description Shows the cookie banner
- */
-function mostrarCookieBanner(){
-  let cookieBanner = document.getElementById("cb-cookie-banner");
-  cookieBanner.style.display = "block";
-}
-
-/**
- * @description Hides the Cookie banner and saves the value to localstorage
- */
-function ocultarCookieBanner(){
-  localStorage.setItem("cb_isCookieAccepted", "yes");
-  let cookieBanner = document.getElementById("cb-cookie-banner");
-  cookieBanner.style.display = "none";
-}
-
-/**
- * @description Checks the localstorage and shows Cookie banner based on it.
- */
-function inicializarCookieBanner(){
-  let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
-  if(isCookieAccepted === null)  {
-    localStorage.setItem("cb_isCookieAccepted", "no");
-    mostrarCookieBanner();
+document.addEventListener("DOMContentLoaded", function () {
+  /**
+   * @description Muestra el Cookie Banner
+   */
+  function mostrarCookieBanner(){
+    let cookieBanner = document.getElementById("cb-cookie-banner");
+    cookieBanner.style.display = "block";
   }
-  if(isCookieAccepted === "no") {
-    mostrarCookieBanner();
-  }
-}
 
-// Assigning values to window object
-window.onload = inicializarCookieBanner();
-window.cb_hideCookieBanner = ocultarCookieBanner;
+  /**
+   * @description Oculta el Cookie Banner y mete el valor en LocalStorage
+   */
+  function ocultarCookieBanner(){
+    localStorage.setItem("cb_isCookieAccepted", "yes");
+    let cookieBanner = document.getElementById("cb-cookie-banner");
+    cookieBanner.style.display = "none";
+  }
+
+  /**
+   * @description Comprueba el LocalStorage y muestra el Cookie Banner dependiendo de ello
+   */
+  function inicializarCookieBanner(){
+    let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
+    if(isCookieAccepted === null)  {
+      localStorage.setItem("cb_isCookieAccepted", "no");
+      mostrarCookieBanner();
+    }
+    if(isCookieAccepted === "no") {
+      mostrarCookieBanner();
+    }
+  }
+
+  window.onload = inicializarCookieBanner();
+  window.cb_hideCookieBanner = ocultarCookieBanner;
+});
+
+
